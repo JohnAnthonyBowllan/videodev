4a0d5fb45735b0b037360bb6d6fa9260c727b0e4
John Bowllan <johnbowllan@John-Bowllan.local>
2023-02-25 19:07:59 -0500

    Test new run config

---
+++ animation_generator.py
@@ -0,0 +1,16 @@
+import os
+from enum import Enum
+
+
+class Resolution(str, Enum):
+    LOW = "l"
+    MEDIUM = "m"
+    HIGH = "h"
+
+
+if __name__ == "__main__":
+    class_name = os.getenv("CLASS")
+    file = os.getenv("FILE_TO_RUN")
+    res = Resolution(os.getenv("RESOLUTION"))
+
+    os.system(f"manim -pq{res.value} {file} {class_name}")
---
+++ backprop/loss_function.py
@@ -0,0 +1,13 @@
+from manim import PI, Circle, Create, FadeOut, Scene, Square, Transform
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
